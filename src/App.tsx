725ff11e6f1c37d32746c6fcdd1cbe6580472858
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-16 10:55:24 +0100

    First changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.scss";
 import carsJson from "./assets/cars.json";
+import { reducer } from "./reducer";
 import { Car } from "./types/car";
 
 function App() {
-  const [cars, setCars] = useState(carsJson as [Car]);
+  const [cars, setCars] = useState<[Car]>(carsJson as [Car]);
+  const [state, dispatch] = useReducer(reducer, dispatch);
+  // const {cars} = state
 
   return (
     <div className="Cars">
       <div className="cars">
-        {cars.map((car) => (
+        {state.cars.map((car) => (
           <div key={car.id} className="car">
             <div className="name">{car.name}</div>
             <div className="name">{car.year}</div>
