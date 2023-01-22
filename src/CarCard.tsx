39bf3fdb9ac2887d4f9ebfb29cbd48d10b5240be
elis-11 <eliza.arzanukaeva@gmail.com>
2023-01-22 21:01:35 +0100

    Update

--- src/CarCard.tsx
+++ src/CarCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ActionsAll } from "./types/action";
 import { Car } from "./types/car";
 import { BsTrash } from "react-icons/bs";
--- src/types/state.ts
+++ src/types/state.ts
@@ -1,6 +1,7 @@
 import { Car } from "./car";
 
 export type State ={
+    // cars: Array<Car>,
     cars: Car[],
     selectedYear: number | undefined,
     filteredYears: number[],
